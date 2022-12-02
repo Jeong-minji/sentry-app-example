7b0daf08380710979efdcbe1b36a6c55328c4ec8
Daisy <daisy@Daisyui-Macmini.local>
2022-12-02 14:09:51 +0900

    Add: Error case

---
+++ pages/login.tsx
@@ -0,0 +1,12 @@
+import { ChangeEvent, useEffect, useState } from "react";
+
+const Login = () => {
+  const value: any = null;
+
+  // error case
+  const { id, password } = value;
+
+  return <input />;
+};
+
+export default Login;
