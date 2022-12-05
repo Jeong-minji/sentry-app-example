24d50b5d8592c01f48c61e541689b03673d89343
Daisy <daisy@Daisyui-Macmini.local>
2022-12-05 16:07:30 +0900

    Add: Only in production mode

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,7 +1,9 @@
 import * as Sentry from "@sentry/nextjs";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN_KEY,
+  dsn: isProduction ? process.env.NEXT_PUBLIC_SENTRY_DSN_KEY : "",
   environment: process.env.NODE_ENV,
   tracesSampleRate: 1.0,
 });
